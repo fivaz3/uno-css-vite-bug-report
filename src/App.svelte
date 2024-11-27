4a682f75265ba233cfc0de99790f9b566dbe1968
fivaz <s.a.fivaz@gmail.com>
2024-11-28 00:39:20 +0100

    refactor

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,9 @@
 <div class="h-28 w-28 shadow">test</div>
 
-<div class="test h-28 w-28">test</div>
+<div class="h-28 w-28 tailwind-shadow">test</div>
 
 <style>
-    .test {
+    .tailwind-shadow {
         box-shadow:
                 0 1px 3px 0 rgb(0 0 0 / 0.1),
                 0 1px 2px -1px rgb(0 0 0 / 0.1);
